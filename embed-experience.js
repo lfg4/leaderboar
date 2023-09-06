65d0826e07c918e5746463a219540c6429d1ec60
Laura Fernández García <laura.fernandez@onirix.com>
2023-09-06 11:20:44 +0200

    .

--- embed-experience.js
+++ embed-experience.js
@@ -8,7 +8,7 @@ const html = `
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap');
     </style>
-    <link rel="stylesheet" type="text/css" href="embed-experience.css">
+    <link rel="stylesheet" type="text/css" href="./embed-experience.css">
     <script type="module" defer src="firebase.js" ></script>
     <script type="module" defer src="embed-experience.js" ></script>
 </head>
@@ -112,6 +112,7 @@ class OxLeaderBoard {
     userSVG = `<svg width="10" height="12" viewBox="0 0 10 12" xmlns="http://www.w3.org/2000/svg"><g fill="#580088" fill-rule="evenodd"><path d="M5.055 5.928c1.584 0 2.872-1.33 2.872-2.964S6.64 0 5.055 0C3.47 0 2.182 1.33 2.182 2.964c0 1.635 1.289 2.964 2.873 2.964M5 6.24c-2.757 0-5 2.315-5 5.16 0 .331.26.6.582.6h8.836a.591.591 0 0 0 .582-.6c0-2.845-2.243-5.16-5-5.16"/></g></svg>`;
 
     constructor() {
+        console.log(this.firebaseController)
         this.includeHTML()
         this.main = document.getElementById("oex-main");
         this.leaderboard = document.querySelector(".oex-leaderboard");
