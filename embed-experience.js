0cecd6db1ce53ec30fc3c1121d24a65adfe79a2e
Laura Fernández García <laura.fernandez@onirix.com>
2023-09-06 12:14:51 +0200

    .

--- embed-experience.js
+++ embed-experience.js
@@ -136,7 +136,31 @@ class OxLeaderBoard {
     includeHTML() {
         // document.getElementById("ox-leaderboard").innerHTML = html
         // $("#ox-leaderboard").load("leaderboard.html");
-        document.getElementById("ox-leaderboard").load("leaderboard.html")
+        // document.getElementById("ox-leaderboard").load("leaderboard.html")
+        var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+        }
+      }
+      xhttp.open("GET", "https://lfg4.github.io/leaderboar/"+file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
     }
     /**
      * Create the iframe that will link the experience
