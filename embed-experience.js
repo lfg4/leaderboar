19fe077af2a23cdd99665cc156d1afc1b3f684ed
Laura Fernández García <laura.fernandez@onirix.com>
2023-09-06 12:29:50 +0200

    .

--- embed-experience.js
+++ embed-experience.js
@@ -134,33 +134,8 @@ class OxLeaderBoard {
     }
 
     includeHTML() {
-        // document.getElementById("ox-leaderboard").innerHTML = html
-        // $("#ox-leaderboard").load("leaderboard.html");
-        // document.getElementById("ox-leaderboard").load("leaderboard.html")
-        var z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("w3-include-html");
-        }
-      }
-      xhttp.open("GET", "https://lfg4.github.io/leaderboar/"+file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
-    }
-  }
+        document.getElementById("ox-leaderboard").innerHTML = html
+        
     }
     /**
      * Create the iframe that will link the experience
@@ -184,6 +159,7 @@ class OxLeaderBoard {
      * If there isn't go to register.
      */
     checkCredentials() {
+        console.log('check')
         this.userCredential = JSON.parse(localStorage.getItem("oex-user"));
         if (this.userCredential) {
             this.getUserData().then(() => this.loadExperience());
